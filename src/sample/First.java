57b047760e7945833fa5d6ffcb9792b71708d9da
Samim Aktar <samimaktar0773@gmail.com>
2021-10-03 12:07:11 +0530

    Done UC2

--- src/sample/First.java
+++ src/sample/First.java
@@ -3,6 +3,13 @@ public class First {
 	public static void main(String [] args) {
 		System.out.println("hello World print");
 		
+		
+		System.out.println("UC1");
+		
+		System.out.println("UC2");
+		
+		
+		
 
 	}
 
